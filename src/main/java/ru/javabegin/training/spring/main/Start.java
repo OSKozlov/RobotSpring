5401086f0b24266aa35406a83af6a2426b6a5ad7
dp070782kos <dp070782kos@gmail.com>
2018-09-24 23:08:15 +0300

    method injection

--- src/main/java/ru/javabegin/training/spring/main/Start.java
+++ src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -4,17 +4,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ru.javabegin.training.spring.impls.robot.ModelT1000;
+import ru.javabegin.training.spring.interfaces.Robot;
+import ru.javabegin.training.spring.interfaces.RobotConveyor;
 
 public class Start {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-//		System.out.println(t1000);
-		t1000.fire();
+		/*ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
+		t1000.fire();*/
 		
+		RobotConveyor t1000Conveyor = (RobotConveyor) context.getBean("t1000Conveyor");
+		Robot terminator1 = t1000Conveyor.createRobot();
+		Robot terminator2 = t1000Conveyor.createRobot();
+		Robot terminator3 = t1000Conveyor.createRobot();
 		
-/*		t1000 = (ModelT1000) context.getBean("t1000");
-		System.out.println(t1000);*/
+		System.out.println("terminator1 " + terminator1);
+		System.out.println("terminator2 " + terminator2);
+		System.out.println("terminator3 " + terminator3);
+		
 	}
 }
